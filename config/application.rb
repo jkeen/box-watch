8f786004b85664a7f683888906e1fea06fade189
Jeff Keen <jeff@keen.me>
2011-01-29 12:40:34 -0600

    Removed prototype, added jQuery

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module PackageTracker
   class Application < Rails::Application
+    # Added by the Rails 3 jQuery Template
+	  # http://github.com/lleger/Rails-3-jQuery, written by Logan Leger
+	  config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+	  config.action_view.javascript_expansions[:cdn] = %w(https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js rails)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -31,7 +36,7 @@ module PackageTracker
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
