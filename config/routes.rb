9f9eafed436afbb9d424ac658bfabac742eac258
Jeff Keen <jeff@spiceworks.com>
2013-04-12 23:48:13 -0500

    Remove old admin stuff

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,6 @@ PackageTracker::Application.routes.draw do
 
   mount Sidekiq::Web, at: "/sidekiq"
 
-
-  namespace "admin" do
-    match "/overview" => "overview#index"
-    match "/overview/:id" => "overview#show"
-
-    root :to => "overview#index"
-  end
-
   match '/:tracking_number' => 'shipments#show'
   root :to => "shipments#new"
 end
--- test/unit/helpers/admin/overview_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Admin::OverviewHelperTest < ActionView::TestCase
-end
